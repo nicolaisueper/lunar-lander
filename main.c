2c073a5c6d8d55ad4c9fb8469405f1da46e5a985
Lukas <k3ll4gr0up@gmail.com>
2014-02-04 11:19:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,27 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{	int ch;
-
- char title[]="Lunar Lander - Version 0.0.1";
- char license[]="Licensed under the GPLv2.";
- char start[]="Press any key to start.";
- int row,col;											                    /* to store the number of rows and */
-														                          /* the number of colums of the screen, */
- initscr();												                    /* start the curses mode, */
- getmaxyx(stdscr,row,col);								            /* get the number of rows and columns, */
- mvprintw(row/2,(col-strlen(title))/2,"%s",title);		/* print the message at the center of the screen */
- mvprintw(row-2,(col-strlen(license))/2,"%s",license);
- mvprintw(row-1,(col-strlen(start))/2,"%s",start);
- refresh();
- getch();
- clear();
- printw("Hier kommt dann das Programm rein!");
- refresh();
- getch();
- endwin();
-
- return 0;
-
+int main(){	
+	int ch;
+	char title[]="Lunar Lander - Version 0.0.1";
+	char license[]="Licensed under the GPLv2.";
+	char start[]="Press any key to start.";
+	int row,col;							/* to store the number of rows and */
+							                /* the number of colums of the screen, */
+	initscr();					                /* start the curses mode, */
+	getmaxyx(stdscr,row,col);					/* get the number of rows and columns, */
+	mvprintw(row/2,(col-strlen(title))/2,"%s",title);		/* print the message at the center of the screen */
+	mvprintw(row-2,(col-strlen(license))/2,"%s",license);
+	mvprintw(row-1,(col-strlen(start))/2,"%s",start);
+	refresh();
+	getch();
+	endwin();	/* End Curses Mode.*/
+	printf("THIS IS A COMPUTER SIMULATION OF AN APOLLO LUNAR LANDING CAPSULE.");
+	printf("THE ON-BOARD COMPUTER HAS FAILED SO YOU HAVE TO LAND THE CAPSULE MANUALLY.");
+	printf("SET BURN RATE OF RETRO ROCKETS TO ANY VALUE BETWEEN 0 (FREE FALL) AND 200 (MAXIMUM BURN) POUNDS PER SECOND.");
+	printf("SET NEW BURN RATE EVERY 10 SECONDS.");
+	printf("CAPSULE WEIGHT 32,500 LBS; FUEL WEIGHT 16,500 LBS.");
+	return 0;
 }
